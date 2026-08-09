6b91bb15f795bd93049137db0e8593d464d8346a
𝚜𝚜@𝚏𝚎𝚍𝚘𝚛𝚊:~/𝚍𝚎𝚟$ █ <101474875+faangbait@users.noreply.github.com>
2026-08-09 08:34:05 -0500

    add tf=terraform alias (#17)

--- test/terraform/ubi10-dnf.sh
+++ test/terraform/ubi10-dnf.sh
@@ -5,6 +5,7 @@ source dev-container-features-test-lib
 
 check "dnf base image is in use" dnf --version
 check "Terraform 1.15.8 is installed" sh -c "terraform version -json | grep -q '\"terraform_version\": \"1.15.8\"'"
+check "Terraform tf alias is configured" grep -Fxq "alias tf=terraform" /etc/profile.d/tf-alias.sh
 check "TFLint 0.64.0 is installed" sh -c "tflint --version | grep -q 'TFLint version 0.64.0'"
 check "Terraform MCP Server 1.1.0 is installed" sh -c "terraform-mcp-server --version | grep -q '1.1.0'"
 
